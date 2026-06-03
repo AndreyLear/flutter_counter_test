b4d179883df6fcfa2e06446a27127e62fdad5a20
AndreyLear <34810848+AndreyLear@users.noreply.github.com>
2026-06-04 02:37:19 +1000

    add container

--- lib/main.dart
+++ lib/main.dart
@@ -45,27 +45,6 @@ class Profile {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final Profile _andreyProfile = Profile(
-    name: 'Andrey',
-    specialization: 'UX/UI Designer',
-    tools: ['Figma', 'Flutter', 'Photoshop'],
-    experienceYears: 8,
-  );
-
-  String _getGrade(int experience) {
-    if (experience < 1) {
-      return 'Noob';
-    } else if (experience < 3) {
-      return 'Junior';
-    } else if (experience < 6) {
-      return 'Middle';
-    } else if (experience < 10) {
-      return 'Senior';
-    } else {
-      return 'Legend';
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -73,102 +52,7 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Padding(
-          padding: EdgeInsets.only(top: 24, left: 24, right: 24),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Text(
-                    'Name:',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: .w600,
-                      letterSpacing: 0,
-                    ),
-                  ),
-                  Spacer(),
-                  Text(
-                    _andreyProfile.name,
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: .w800,
-                      letterSpacing: 0,
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 8),
-              Row(
-                children: [
-                  Text(
-                    'Specialization:',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: .w600,
-                      letterSpacing: 0,
-                    ),
-                  ),
-                  Spacer(),
-                  Text(
-                    _andreyProfile.specialization,
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: .w800,
-                      letterSpacing: 0,
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 8),
-              Row(
-                children: [
-                  Text(
-                    'Tools:',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: .w600,
-                      letterSpacing: 0,
-                    ),
-                  ),
-                  Spacer(),
-                  Text(
-                    _andreyProfile.tools.join(','),
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: .w800,
-                      letterSpacing: 0,
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 8),
-              Row(
-                children: [
-                  Text(
-                    'Experience: ',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: .w600,
-                      letterSpacing: 0,
-                    ),
-                  ),
-                  Spacer(),
-                  Text(
-                    _getGrade(_andreyProfile.experienceYears),
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: .w800,
-                      letterSpacing: 0,
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
+      body: Center(child: Container(height: 100, color: Colors.blue)),
     );
   }
 }
